ca1c6bdb889fc9113ae44213a99068083fa7efb2
Neekhil Verma <neekhil18@users.noreply.github.com>
2018-10-24 20:26:34 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+import keras
+from keras.models import Sequential,Input,Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
+
+batch_size = 64
+epochs = 20
+num_classes = 10
+
+
+fashion_model = Sequential()
+fashion_model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(28,28,1),padding='same'))
+fashion_model.add(LeakyReLU(alpha=0.1))
